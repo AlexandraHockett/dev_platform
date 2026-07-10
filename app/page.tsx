055a3bd175fee0ce541643e7775b6ea77c5ef5f7
Alexandra Hockett <contact@ahockett.com>
2026-07-10 14:27:51 +0100

    Add Prettier with Tailwind plugin and format existing files

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const Page = () => {
   return (
-    <div><h1>Create a Next.js App</h1></div>
-  )
-}
+    <div>
+      <h1>Create a Next.js App</h1>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
