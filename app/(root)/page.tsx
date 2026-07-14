ae1c1b15c53af14a873c8b2281dc77db7346f19b
Alexandra Hockett <contact@ahockett.com>
2026-07-14 01:42:10 +0100

    Remove temporary logout button from home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,4 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/route";
+import { auth } from "@/auth";
 
 const Home = async () => {
   const session = await auth();
@@ -9,18 +7,6 @@ const Home = async () => {
   return (
     <div>
       <h1>Create a Next.js App</h1>
-
-      <form
-        className="px-10 pt-25"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit" variant="glow" size="lg">
-          Log out
-        </Button>
-      </form>
     </div>
   );
 };
